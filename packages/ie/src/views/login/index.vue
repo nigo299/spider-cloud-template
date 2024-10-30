e80eda62bda7cf3d35da024b40a371a4d28eeeb6
nigo299 <chenzhuo9729@gmail.com>
2024-10-30 10:21:15 +0800

    ✨feat: 收拢lint配置

--- packages/ie/src/views/login/index.vue
+++ packages/ie/src/views/login/index.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import {useToggle} from '@vueuse/core'
-import {onMounted, reactive, ref} from 'vue'
-import {to} from '@/utils'
-import router from '@/router'
-import {getToken, login} from '@/api/login'
-import {TOKEN_KEY} from '@/common/js/config'
+import { useToggle } from '@vueuse/core'
+import useSWRV from 'swrv'
+import { onMounted, reactive, ref } from 'vue'
+
+import { getToken, login } from '@/api/login'
+import { disableSession } from '@/api/user'
+import { TOKEN_KEY } from '@/common/js/config'
 import CopyRight from '@/components/CopyRight/index.vue'
-import type {LoginParam} from '@/interface/login'
-import useSWRV from "swrv";
-import {disableSession} from "@/api/user";
-import {NotRevalidateOption} from "@/utils/swrv";
+import type { LoginParam } from '@/interface/login'
+import router from '@/router'
+import { to } from '@/utils'
+import { NotRevalidateOption } from '@/utils/swrv'
 
 const formState = reactive<LoginParam>({
   account: '',
@@ -27,6 +28,7 @@ function onFinish() {
 
 async function buildLogin() {
   const [data, err] = await to(getToken())
+
   if (!err) {
     sessionStorage.setItem(TOKEN_KEY, data.token)
   }
@@ -41,11 +43,12 @@ onMounted(async () => {
 
 async function loginIn() {
   submitToggle(true)
-  let loginParam = {...formState}
+  let loginParam = { ...formState }
+
   if (import.meta.env.MODE === 'development' || import.meta.env.MODE === 'production') {
     loginParam = {
       ...formState,
-      password: import.meta.env.VITE_APP_ENCRYPTPASSWORD
+      password: import.meta.env.VITE_APP_ENCRYPTPASSWORD,
     }
   }
   const [data, err] = await to(login(loginParam))
@@ -54,7 +57,8 @@ async function loginIn() {
     sessionStorage.setItem(TOKEN_KEY, data.token)
     submitToggle(false)
     router.replace('/indicatorDashboard')
-  } else {
+  }
+  else {
     submitToggle(false)
   }
 }
@@ -63,16 +67,16 @@ async function loginIn() {
 <template>
   <div>
     <el-row v-if="!isProd" class="min-h-screen flex">
-      <el-col :span="12" class="bg-[#00706b] h-screen"/>
+      <el-col :span="12" class="bg-[#00706b] h-screen" />
       <el-col :span="12" class="bg-[#ffffff] relative">
         <div class="w-full h-full flex justify-center items-center">
           <el-form class="w-1/2" :model="formState" name="basic" autocomplete="off" @keyup.enter.native="onFinish">
             <el-form-item prop="account" :rules="[{ required: true, message: '请输入账号' }]">
-              <el-input v-model="formState.account" size="large" clearable placeholder="请输入账号"/>
+              <el-input v-model="formState.account" size="large" clearable placeholder="请输入账号" />
             </el-form-item>
 
             <el-form-item prop="password" :rules="[{ required: true, message: '请输入密码' }]">
-              <el-input v-model="formState.password" type="password" size="large" clearable placeholder="请输入密码"/>
+              <el-input v-model="formState.password" type="password" size="large" clearable placeholder="请输入密码" />
             </el-form-item>
             <el-form-item>
               <el-button size="large" class="w-full" round type="primary" :loading="submit" @click="onFinish">
@@ -82,7 +86,7 @@ async function loginIn() {
           </el-form>
         </div>
         <div class="absolute bottom-8 w-full flex justify-center">
-          <CopyRight/>
+          <CopyRight />
         </div>
       </el-col>
     </el-row>
