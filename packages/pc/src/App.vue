a4060760291c74a262e8ed545ad046edbd7c70cc
nigo299 <chenzhuo9729@gmail.com>
2024-11-08 15:58:39 +0800

    ✨feat: 全局主题配置优化

--- packages/pc/src/App.vue
+++ packages/pc/src/App.vue
@@ -4,22 +4,15 @@ import zhCN from 'ant-design-vue/es/locale/zh_CN'
 import { RouterView } from 'vue-router'
 
 import { layoutConfig } from './config/layout'
+import { themeConfig } from './config/theme'
 </script>
 
 <template>
   <a-config-provider
     :locale="zhCN"
     :theme="{
-      token: {
-        colorPrimary: '#00706b',
-        colorPrimaryBg: '#9fd4c8',
-        colorSuccess: '#5ec76f',
-        colorWarning: '#efb33f',
-        colorError: '#ec655f',
-        colorInfo: '#00706b',
-        borderRadius: 4,
-        wireframe: true,
-      } }"
+      token: themeConfig.light,
+    }"
   >
     <RouterView v-slot="{ Component }">
       <keep-alive v-if="layoutConfig.tabConfig.keepAlive">
